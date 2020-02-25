e7a48d5ddef87a2513be0e7d454b263b06b8a619
Marcus Johnsen <cph-mj734@cphbusiness.dk>
2020-02-25 09:05:32 +0100

    starting on the assignment

---
+++ 25-02 Tuesday/JPARelations/src/main/java/entity/Customer.java
@@ -0,0 +1,64 @@
+package entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private List<String> hobbies = new ArrayList();
+
+    public List addHobby(String s) {
+        
+        return hobbies;
+    }
+    
+    public String getHobbies() {
+        
+        return hobbies.get(0);
+    }
+    
+    
+    public Customer(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Customer() {}
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
